df29ecf7dad019ef76264b9816d94ace7e5aa35c
Josh M <musto.josh@gmail.com>
2022-10-27 12:02:36 -0400

    Updated JavaScript with randomized topic selection

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
 var topics=["HTML", "CSS", "Git", "JavaScript"];
-for (var t=0; t<topics.length; t++){
-    console.log(topics[t]);
+var randomTopic = topics[Math.floor(Math.random()*topics.length)];
+
+function listTopics(){
+    for (var t=0; t<topics.length; t++){
+        console.log(topics[t]);
+    }
 }
 
-/*
-if (topic==='HTML') {
-    console.log("Let's study HTML!");
-} else if(topic === 'CSS'){
-    console.log("Let's study CSS!");
-} else if(topic==='Git'){
-    console.log("Let's study Git!");
-} else if(topic==='JavaScript'){
-    console.log("Let's study JavaScript!");
-} else{
-    console.log('Please try again!');
+function selectTopic(){
+    if (randomTopic==='HTML') {
+        console.log("Let's study HTML!");
+    } else if(randomTopic === 'CSS'){
+        console.log("Let's study CSS!");
+    } else if(randomTopic==='Git'){
+        console.log("Let's study Git!");
+    } else if(randomTopic==='JavaScript'){
+        console.log("Let's study JavaScript!");
+    } else{
+        console.log('Please try again!');
+    }
 }
-*/
+
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
